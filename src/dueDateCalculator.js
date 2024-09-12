19cdc6da9178fd3e53d01b9c7b48ea7ab2ec805f
busache <ian.shane.sears@gmail.com>
2024-09-12 09:39:02 -0400

    Implements basic due date calculation logic and test

--- src/dueDateCalculator.js
+++ src/dueDateCalculator.js
@@ -1,1 +1,7 @@
-module.exports = {};
+function calculateDueDate(submitTime, turnaroundTime) {
+  const dueDate = new Date(submitTime);
+  dueDate.setHours(dueDate.getHours() + turnaroundTime);
+  return dueDate;
+}
+
+module.exports = { calculateDueDate };
--- tests/dueDateCalculator.test.js
+++ tests/dueDateCalculator.test.js
@@ -1,1 +1,16 @@
-// dueDateCalculator.test.js placeholder for future tests
+const { calculateDueDate } = require("../src/dueDateCalculator");
+const assert = require("assert");
+
+// Simple test case
+function runTest(submitTime, turnaroundTime, expectedDueDate) {
+  const actualDueDate = calculateDueDate(submitTime, turnaroundTime);
+  assert.strictEqual(
+    actualDueDate.getTime(),
+    expectedDueDate.getTime(),
+    `Expected ${expectedDueDate}, but got ${actualDueDate}`
+  );
+  console.log("Test passed!");
+}
+
+// Test case
+runTest(new Date("2024-09-11T09:00:00Z"), 8, new Date("2024-09-11T17:00:00Z"));
