d4663ac3866619dba0365fa83b5e73298cb526ea
busache <ian.shane.sears@gmail.com>
2024-09-12 09:53:02 -0400

    Adds logic to restrict due date to working hours

--- src/dueDateCalculator.js
+++ src/dueDateCalculator.js
@@ -1,6 +1,33 @@
+const WORK_START_HOUR = 9;
+const WORK_END_HOUR = 17;
+
 function calculateDueDate(submitTime, turnaroundTime) {
-  const dueDate = new Date(submitTime);
-  dueDate.setHours(dueDate.getHours() + turnaroundTime);
+  let dueDate = new Date(submitTime);
+  let remainingHours = turnaroundTime;
+
+  while (remainingHours > 0) {
+    const currentHour = dueDate.getUTCHours();
+    
+    // Check if we are within working hours
+    if (currentHour >= WORK_START_HOUR && currentHour < WORK_END_HOUR) {
+      const availableHoursToday = WORK_END_HOUR - currentHour;
+
+      if (remainingHours <= availableHoursToday) {
+        dueDate.setUTCHours(currentHour + remainingHours);
+        remainingHours = 0;
+      } else {
+        remainingHours -= availableHoursToday;
+        dueDate.setUTCHours(WORK_END_HOUR); // End of working day (5 PM)
+      }
+    }
+
+    // Move to the next working day
+    if (remainingHours > 0) {
+      dueDate.setUTCDate(dueDate.getUTCDate() + 1);
+      dueDate.setUTCHours(WORK_START_HOUR); // Reset to start of workday (9 AM)
+    }
+  }
+
   return dueDate;
 }
 
--- tests/dueDateCalculator.test.js
+++ tests/dueDateCalculator.test.js
@@ -1,16 +1,15 @@
-const { calculateDueDate } = require("../src/dueDateCalculator");
-const assert = require("assert");
+const { calculateDueDate } = require('../src/dueDateCalculator');
+const assert = require('assert');
 
-// Simple test case
+// Helper function to run tests
 function runTest(submitTime, turnaroundTime, expectedDueDate) {
   const actualDueDate = calculateDueDate(submitTime, turnaroundTime);
-  assert.strictEqual(
-    actualDueDate.getTime(),
-    expectedDueDate.getTime(),
-    `Expected ${expectedDueDate}, but got ${actualDueDate}`
-  );
-  console.log("Test passed!");
+  assert.strictEqual(actualDueDate.getTime(), expectedDueDate.getTime(), `Expected ${expectedDueDate}, but got ${actualDueDate}`);
+  console.log('Test passed!');
 }
 
-// Test case
-runTest(new Date("2024-09-11T09:00:00Z"), 8, new Date("2024-09-11T17:00:00Z"));
+// Test same-day turnaround within working hours
+runTest(new Date(Date.UTC(2024, 8, 11, 9, 0)), 8, new Date(Date.UTC(2024, 8, 11, 17, 0)));
+
+// Test for spilling over to the next working day
+runTest(new Date(Date.UTC(2024, 8, 11, 16, 0)), 2, new Date(Date.UTC(2024, 8, 12, 10, 0)));
