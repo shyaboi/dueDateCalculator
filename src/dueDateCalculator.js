10aaaa86a053be44ab6a9a0916c501c1cefb80ac
busache <ian.shane.sears@gmail.com>
2024-09-12 09:56:56 -0400

    updates logic to skip weekends for due date, adds feature branch

--- src/dueDateCalculator.js
+++ src/dueDateCalculator.js
@@ -7,8 +7,16 @@ function calculateDueDate(submitTime, turnaroundTime) {
 
   while (remainingHours > 0) {
     const currentHour = dueDate.getUTCHours();
-    
-    // Check if we are within working hours
+    const currentDay = dueDate.getUTCDay(); // Sunday = 0, Saturday = 6
+
+    // Skip weekends
+    if (currentDay === 0 || currentDay === 6) {
+      dueDate.setUTCDate(dueDate.getUTCDate() + 1);
+      dueDate.setUTCHours(WORK_START_HOUR); // Reset to 9:00 AM
+      continue;
+    }
+
+    // If within working hours
     if (currentHour >= WORK_START_HOUR && currentHour < WORK_END_HOUR) {
       const availableHoursToday = WORK_END_HOUR - currentHour;
 
@@ -17,14 +25,14 @@ function calculateDueDate(submitTime, turnaroundTime) {
         remainingHours = 0;
       } else {
         remainingHours -= availableHoursToday;
-        dueDate.setUTCHours(WORK_END_HOUR); // End of working day (5 PM)
+        dueDate.setUTCHours(WORK_END_HOUR); // End of the working day (5:00 PM)
       }
     }
 
     // Move to the next working day
     if (remainingHours > 0) {
       dueDate.setUTCDate(dueDate.getUTCDate() + 1);
-      dueDate.setUTCHours(WORK_START_HOUR); // Reset to start of workday (9 AM)
+      dueDate.setUTCHours(WORK_START_HOUR); // Reset to start of workday (9:00 AM)
     }
   }
 
